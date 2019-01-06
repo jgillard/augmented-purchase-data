79e3663b0562f364f475d086c1bbefc440d59e36
James Gillard <jamesgillard@live.co.uk>
2019-01-06 17:18:16 +0700

    dep

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/jgillard/practising-go-tdd"
+	transactioncategories "github.com/jgillard/practising-go-tdd"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	categoryStore := transactioncategories.NewInMemoryCategoryStore()
 	questionStore := transactioncategories.NewInMemoryQuestionStore()
 
 	server := transactioncategories.NewServer(categoryStore, questionStore)
 
-	if err := http.ListenAndServe(":5000", server); err != nil {
-		log.Fatalf("could not listen on port 5000 %v", err)
+	if err := http.ListenAndServe(":"+port, server); err != nil {
+		log.Fatalf("could not listen on port %s %v", port, err)
 	}
 }
